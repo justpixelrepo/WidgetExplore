7aee2bd3eb84a17b1d585c8371ca5261d50b2895
codebendr-product-designer <35074528+codebendr-product-designer@users.noreply.github.com>
2023-10-09 10:56:32 -0400

    new setup

---
+++ WidgetInsights/WidgetInsights.swift
@@ -0,0 +1,87 @@
+//
+//  WidgetInsights.swift
+//  WidgetInsights
+//
+//  Created by Evans Domina Attafuah on 09/10/2023.
+//
+
+import WidgetKit
+import SwiftUI
+
+struct Provider: TimelineProvider {
+    func placeholder(in context: Context) -> SimpleEntry {
+        SimpleEntry(date: Date(), emoji: "😀")
+    }
+
+    func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> ()) {
+        let entry = SimpleEntry(date: Date(), emoji: "😀")
+        completion(entry)
+    }
+
+    func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
+        var entries: [SimpleEntry] = []
+
+        // Generate a timeline consisting of five entries an hour apart, starting from the current date.
+        let currentDate = Date()
+        for hourOffset in 0 ..< 5 {
+            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
+            let entry = SimpleEntry(date: entryDate, emoji: "😀")
+            entries.append(entry)
+        }
+
+        let timeline = Timeline(entries: entries, policy: .atEnd)
+        completion(timeline)
+    }
+}
+
+struct SimpleEntry: TimelineEntry {
+    let date: Date
+    let emoji: String
+}
+
+struct WidgetInsightsEntryView : View {
+    var entry: Provider.Entry
+
+    var body: some View {
+        VStack {
+            Text("Time:")
+            Text(entry.date, style: .time)
+
+            Text("Emoji:")
+            Text(entry.emoji)
+        }
+    }
+}
+
+struct WidgetInsights: Widget {
+    let kind: String = "WidgetInsights"
+
+    var body: some WidgetConfiguration {
+        StaticConfiguration(kind: kind, provider: Provider()) { entry in
+            if #available(iOS 17.0, *) {
+                WidgetInsightsEntryView(entry: entry)
+                    .containerBackground(.fill.tertiary, for: .widget)
+            } else {
+                WidgetInsightsEntryView(entry: entry)
+                    .padding()
+                    .background()
+            }
+        }
+        .configurationDisplayName("My Widget")
+        .description("This is an example widget.")
+    }
+}
+
+#Preview(as: .systemSmall) {
+    WidgetInsights()
+} timeline: {
+    SimpleEntry(date: .now, emoji: "😀")
+    SimpleEntry(date: .now, emoji: "🤩")
+}
+
+#Preview(as: .systemMedium) {
+    WidgetInsights()
+} timeline: {
+    SimpleEntry(date: .now, emoji: "😀")
+    SimpleEntry(date: .now, emoji: "🤩")
+}
---
+++ WidgetInsights/WidgetInsightsBundle.swift
@@ -0,0 +1,16 @@
+//
+//  WidgetInsightsBundle.swift
+//  WidgetInsights
+//
+//  Created by Evans Domina Attafuah on 09/10/2023.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct WidgetInsightsBundle: WidgetBundle {
+    var body: some Widget {
+        WidgetInsights()
+    }
+}
