857cb41b4b259abd5fcb16f452e74bb223dbdd08
codebendr-product-designer <35074528+codebendr-product-designer@users.noreply.github.com>
2023-10-09 10:01:13 -0400

    Initial Commit

---
+++ WidgetExplore/WidgetExploreApp.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetExploreApp.swift
+//  WidgetExplore
+//
+//  Created by Evans Domina Attafuah on 09/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct WidgetExploreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
